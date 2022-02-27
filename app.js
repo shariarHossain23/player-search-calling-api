6d52a5cb5f3af9fb10ab46e8a5218ff5ebefd1ab
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-27 15:10:57 +0600

    setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const searchPlayer = async () =>{
+    const searchInput = document.getElementById("search-input");
+    const searchValue = searchInput.value;
+    //  clear display 
+    searchInput.value = "";
+
+    // calling api
+    
+}
