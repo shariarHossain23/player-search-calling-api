44eef78192c8ad1e5785ce2287718afdbf493bc5
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-27 15:16:45 +0600

    api call done

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ const searchPlayer = async () =>{
     searchInput.value = "";
 
     // calling api
+    try {
+        const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
+        const res = await fetch(url)
+        const data = await res.json()
+        showDisplay(data.player)
+    } catch (error) {
+        console.log(error)
+    }
     
+}
+
+const showDisplay = players => {
+    console.log(players)
 }
